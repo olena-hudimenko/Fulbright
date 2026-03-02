1a98220e72349491e5799616d9232fafc090811b
Lena Hudimenko <118678974+olena-hudimenko@users.noreply.github.com>
2026-03-02 11:59:09 -0500

    Create Presentation.jsx component

---
+++ Presentation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Presentation = () => {
+    return (
+        <div>
+            <h1>Welcome to the Presentation</h1>
+            <p>This is a simple presentation component.</p>
+        </div>
+    );
+};
+
+export default Presentation;
